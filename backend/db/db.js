77d442406d148532a957fe3215a4fb4126dd6f93
Eric Skaftason <skaftasoneric@gmail.com>
2025-08-18 22:47:35 -0400

    connection pool now uses .env file data

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.ALPHABETA, // db name will be defined; the db must be initialised before running this
     connectionLimit: 10,
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'cinnamon123',
-    database: 'alphabeta', // db name will be defined; the db must be initialised before running this
     debug: false // toggle showing querries
 });
 
