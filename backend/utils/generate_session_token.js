ebd3b31176412059aabf5b1a294debee6a059e82
Eric Skaftason <skaftasoneric@gmail.com>
2025-08-20 18:53:40 -0400

    session token valid characters no longer include special characters

--- backend/utils/generate_session_token.js
+++ backend/utils/generate_session_token.js
@@ -4,7 +4,7 @@ const token_length = config_session_token.LENGTH;
 module.exports = () => {
     let session_token = '';
 
-    const valid_chars = 'qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()<>,./?';
+    const valid_chars = 'qwertyuiopasdfghjklzxcvbnm1234567890';
     
     for (let i = 0; i < token_length; i++) {
         const char = valid_chars[Math.floor(Math.random() * valid_chars.length)];
