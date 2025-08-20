3c272524e3fc3f7fd659322fd38a48b3d671f9f7
Eric Skaftason <skaftasoneric@gmail.com>
2025-08-19 23:31:02 -0400

    fixed naming error with pool arguments, pool now exports the promise version

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.ALPHABETA, // db name will be defined; the db must be initialised before running this
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME, // db name will be defined; the db must be initialised before running this
     connectionLimit: 10,
     debug: false // toggle showing querries
 });
 
-module.exports = pool;
+module.exports = pool.promise();
